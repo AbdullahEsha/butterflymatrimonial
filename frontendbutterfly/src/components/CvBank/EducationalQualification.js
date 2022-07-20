d5fe87900fd248323fd3e89048818b1d5fe29075
AbdullahIsha <shahariaresha@gmail.com>
2022-07-20 11:29:26 +0600

    frontend update cv, backend file name change

--- frontendbutterfly/src/components/CvBank/EducationalQualification.js
+++ frontendbutterfly/src/components/CvBank/EducationalQualification.js
@@ -50,94 +50,116 @@ const EducationalQulification = (props) => {
   return (
     <>
       <Container className="cv_bank_container21">
-        <Row>
-          <Col xs={12} md={6}>
-            <h4>Level of Education:</h4>
-            <select
-              className="form-control"
-              // onChange={(event) =>
-              //   setPersonalInformation({
-              //     ...personalInformation,
-              //     bloodGroup: event.target.value,
-              //   })
-              // }
-            >
-              <option value="" disabled selected>
-                Choose One
-              </option>
-              <option value="Secondary">Secondary</option>
-              <option value="Higher Secondary">Higher Secondary</option>
-              <option value="Diploma">Diploma</option>
-              <option value="Bachelor Honors">Bachelor Honors</option>
-              <option value="Masters">Masters</option>
-              <option value="PhD">PhD(Doctor of philosophy)</option>
-            </select>
-          </Col>
-          <Col xs={0} md={6} className="hideyear">
-            <h4>Concentration/Major/Group</h4>
-            <div className="App2">
-              <Select options={options} />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <div className="positionQualification">
         {educationalQulification.map((item, index) => {
           return (
             <div key={index}>
-              <br />
-              <Container className="cv_bank_container21">
-                <Row>
-                  <Col xs={12} md={6}>
-                    <h4>Institute Name</h4>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Enter your school/college/university name."
-                      name="instituteName"
-                      value={educationalQulification.instituteName}
-                      onChange={(event) => handleInput(index, event)}
-                    />
-                  </Col>
-                  <Col xs={12} md={6}>
-                    <h4>Year of Passing</h4>
-                    <input
-                      type="month"
-                      className="form-control"
-                      name="passingYear"
-                      value={educationalQulification.passingYear}
-                      onChange={(event) => handleInput(index, event)}
-                    />
-                  </Col>
-                </Row>
-              </Container>
-              <Container align="right" className="cv_bank_container21">
-                {educationalQulification.length - 1 === index &&
-                  educationalQulification.length > 1 &&
-                  educationalQulification.length < 5 && (
-                    <button
-                      className="educationAdd"
-                      title="Remove One"
-                      onClick={() => handleServiceRemove(index)}
-                    >
-                      <FaMinus size={15} color="white" />
-                    </button>
-                  )}{' '}
-                {educationalQulification.length - 1 === index &&
-                  educationalQulification.length < 4 && (
-                    <button
-                      className="educationAdd"
-                      title="Add More"
-                      onClick={handleServiceAdd}
-                    >
-                      <FaPlus size={15} color="white" />
-                    </button>
-                  )}
-              </Container>
+              <div className="row-padding">
+                <h4
+                  style={{
+                    borderBottom: '1px solid #ff566b',
+                    color: '#ff566b',
+                  }}
+                >
+                  Academic {index + 1}
+                </h4>
+              </div>
+
+              <Row className="row-padding">
+                <Col xs={12} md={6}>
+                  <h4>Level of Education:</h4>
+                  <select
+                    className="form-control"
+                    // onChange={(event) =>
+                    //   setPersonalInformation({
+                    //     ...personalInformation,
+                    //     bloodGroup: event.target.value,
+                    //   })
+                    // }
+                  >
+                    <option value="" disabled selected>
+                      Choose One
+                    </option>
+                    <option value="Secondary">Secondary</option>
+                    <option value="Higher Secondary">Higher Secondary</option>
+                    <option value="Diploma">Diploma</option>
+                    <option value="Bachelor Honors">Bachelor Honors</option>
+                    <option value="Masters">Masters</option>
+                    <option value="PhD">PhD(Doctor of philosophy)</option>
+                  </select>
+                </Col>
+                <Col xs={0} md={6}>
+                  <h4>Concentration/Major/Group</h4>
+                  <div className="App2">
+                    <Select options={options} />
+                  </div>
+                </Col>
+              </Row>
+              <Row className="row-padding">
+                <Col xs={12} md={6}>
+                  <h4>Institute Name</h4>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter your institute name."
+                    name="instituteName"
+                    value={educationalQulification.instituteName}
+                    onChange={(event) => handleInput(index, event)}
+                  />
+                </Col>
+                <Col xs={12} md={6}>
+                  <h4>Year of Passing</h4>
+                  <input
+                    type="month"
+                    className="form-control"
+                    name="passingYear"
+                    placeholder="mm/yyyy"
+                    value={educationalQulification.passingYear}
+                    onChange={(event) => handleInput(index, event)}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} md={12}>
+                  <h4>Institute Location</h4>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter your institute location."
+                    name="instituteLocation"
+                    value={educationalQulification.instituteName}
+                    onChange={(event) => handleInput(index, event)}
+                  />
+                </Col>
+              </Row>
+              <Row className="row-padding">
+                <Col align="right" className="cv_bank_container21">
+                  {educationalQulification.length - 1 === index &&
+                    educationalQulification.length > 1 &&
+                    educationalQulification.length < 5 && (
+                      <button
+                        className="educationAdd"
+                        title="Remove One"
+                        onClick={() => handleServiceRemove(index)}
+                      >
+                        <FaMinus size={15} color="white" />
+                      </button>
+                    )}{' '}
+                  {educationalQulification.length - 1 === index &&
+                    educationalQulification.length < 4 && (
+                      <button
+                        className="educationAdd"
+                        title="Add More"
+                        onClick={handleServiceAdd}
+                      >
+                        <FaPlus size={15} color="white" />
+                      </button>
+                    )}
+                </Col>
+              </Row>
             </div>
           )
         })}
-      </div>
+      </Container>
     </>
   )
 }
--- frontendbutterfly/src/components/CvBank/ProfessionalQualification.js
+++ frontendbutterfly/src/components/CvBank/ProfessionalQualification.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const ProfessionalQulification = (props) => {
+  const [fromdate, setFromDate] = useState('')
+  const [todate, setToDate] = useState('')
+  const [current, setCurrent] = useState('notChacked')
+
   const [professionalQulification, setProfessionalQulification] = useState({
     designation: '',
     organizationName: '',
@@ -16,31 +20,150 @@ const ProfessionalQulification = (props) => {
   return (
     <>
       <Container className="cv_bank_container21">
-        <h4>Designation:</h4>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter your designation."
-          onChange={(event) =>
-            setProfessionalQulification({
-              ...professionalQulification,
-              designation: event.target.value,
-            })
-          }
-        />
-        <br />
-        <h4>Name Of The Organization:</h4>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter organization name."
-          onChange={(event) =>
-            setProfessionalQulification({
-              ...professionalQulification,
-              organizationName: event.target.value,
-            })
-          }
-        />
+        <Row className="row-padding">
+          <Col xs={12} md={6}>
+            <h4>Designation:</h4>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your designation."
+              onChange={(event) =>
+                setProfessionalQulification({
+                  ...professionalQulification,
+                  designation: event.target.value,
+                })
+              }
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <h4>Company Name:</h4>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter organization name."
+              onChange={(event) =>
+                setProfessionalQulification({
+                  ...professionalQulification,
+                  organizationName: event.target.value,
+                })
+              }
+            />
+          </Col>
+        </Row>
+        <Row className="row-padding">
+          <Col xs={12} md={6}>
+            <h4>Department:</h4>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter department."
+              onChange={(event) =>
+                setProfessionalQulification({
+                  ...professionalQulification,
+                  organizationName: event.target.value,
+                })
+              }
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <h4>Location:</h4>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter location."
+              onChange={(event) =>
+                setProfessionalQulification({
+                  ...professionalQulification,
+                  organizationName: event.target.value,
+                })
+              }
+            />
+          </Col>
+        </Row>
+        <Row className="row-padding">
+          <h4>Employment Period:</h4>
+          <Col xs={12} md={6}>
+            <div class="from-group">
+              <div class="input-group mb-2">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="inlineFormInputGroup"
+                  value={fromdate}
+                  placeholder="From Date(MM/DD/YYYY)"
+                  disabled
+                />
+                <div class="input-group-prepend">
+                  <div class="input-group-text">
+                    <input
+                      type="date"
+                      id="date-change"
+                      onChange={(event) => setFromDate(event.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col xs={12} md={6}>
+            {current === 'notChacked' && (
+              <div class="from-group">
+                <div class="input-group mb-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="inlineFormInputGroup"
+                    value={todate}
+                    placeholder="To Date(MM/DD/YYYY)"
+                    disabled
+                  />
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <input
+                        type="date"
+                        id="date-change"
+                        onChange={(event) => setToDate(event.target.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Col>
+          <div class="checkbox">
+            <label style={{ marginLeft: '20px' }}>
+              <input
+                type="checkbox"
+                value="Chacked"
+                className="form-check-input"
+                onChange={(event) => {
+                  if (event.target.checked) {
+                    setCurrent(event.target.value)
+                  } else if (!event.target.checked) {
+                    setCurrent('notChacked')
+                  }
+                }}
+              />{' '}
+              Currently working here?
+            </label>
+          </div>
+        </Row>
+        <Row>
+          <Col xs={12} md={12}>
+            <h4>Reference:</h4>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter department name."
+              onChange={(event) =>
+                setProfessionalQulification({
+                  ...professionalQulification,
+                  organizationName: event.target.value,
+                })
+              }
+            />
+          </Col>
+        </Row>
       </Container>
     </>
   )
